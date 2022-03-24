042732130ac9a553f0aa9a5ccc8f76614fc9a4fb
dgwiazda <damiangwiazdaa@wp.pl>
2022-03-24 20:47:09 +0100

    zadanko

---
+++ src/Structure.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.Optional;
+
+public interface Structure {
+
+    Optional findBlockByColor(String color);
+
+    List findBlocksByMaterial(String material);
+
+    int count();
+}
